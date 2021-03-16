bcabd3ebaf2f24a5fcf76a9aa628a44b5e2a8b03
Subham Chakraborty <subhamchakraborty01748@gmail.com>
2021-03-16 16:31:00 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import Hompage from './homepage.component'
+
+function App() {
+  return (
+    <div>
+    <Hompage/>
+    </div>
+  );
+}
+
+export default App;
