33c553b060fb16731680251034868652cf576cdd
Subham Chakraborty <subhamchakraborty01748@gmail.com>
2021-04-03 15:56:19 +0530

    added sign in ,form input and button functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 
 const HatsPage = () => {
@@ -24,7 +25,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignUpPage} />
           <Route path="/shop/hats" component={HatsPage} />
+
         </Switch>
       </Router>
     </div>
